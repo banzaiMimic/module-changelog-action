9311ba9dc51d777b083216c6cae4bc50184412a9
drMimic <gartflores@gmail.com>
2020-10-24 17:19:59 -0400

    first action test

---
+++ index.js
@@ -0,0 +1,16 @@
+const core = require('@actions/core')
+const github = require('@actions/github')
+
+try {
+
+  const nameToGreet = core.getInput('who-to-greet')
+  console.log(`Hello ${nameToGreet}!`)
+  const time = (new Date()).toTimeString()
+  core.setOutput('time', time)
+  // get JSON webhook payload for the event that triggered the workflow
+  const payload = JSON.stringify(github.context.payload, undefined, 2)
+  console.log(`The event payload: ${payload}`)
+
+} catch (e) {
+  core.setFailed(e.message)
+}
